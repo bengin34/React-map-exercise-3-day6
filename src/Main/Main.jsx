0fd5ed0808b62a098ca47742f19619d590ea7e8c
bengin34 <becaglar3434@gmail.com>
2023-02-15 14:58:49 +0300

    done

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import {tenHighestPopulation} from '../data'
+import { Progress } from 'antd'
 
 const Main = () => {
 
   return (
-    <div>
+    <div className='container-sm'>
       {tenHighestPopulation.map((item,index) => (
-        <div className='container key={index}'>
+        <div className='container' key={index}>
         <div className='d-flex justify-content-between' key={index}>
-        <div>
-          <h2>{item.country}</h2>
+        <div className='float-sm-start'>
+          <h4 style={{width:"100px"}} >{item.country}</h4>
         </div>
-          <h5>{item.population}</h5>
+        <div style={{width:"30vw"}} ><Progress percent={item.rate} status="active" showInfo={false} /></div>
+        <div ><p>{item.population}</p></div>
+          
         </div>
         </div>
       )
